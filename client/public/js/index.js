d0ba4343ee4398a1c1d0cbc38f90b92f3dca58ce
Ann Quintero <aq4919@students.necsd.net>
2024-07-10 13:13:21 -0400

    appServer.js

--- client/public/js/index.js
+++ client/public/js/index.js
@@ -7,5 +7,17 @@
 function checkKeyPress(e) {
   if (e.keyCode == 32) {
     goToLocation('/feed');
+    
   }
+}
+
+document.addEventListener("keydown", checkKeyPress);
+document.addEventListener("click", function(){
+  goToLocation("/feed");
+});
+
+let fadeText = document.getElementById("fade_text");
+
+fadeText.onclick = () => {
+  window.location = "/feed";
 }
