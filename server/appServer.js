a2e5ac5d1353548eb69f849d9b65648aadbceac3
Ann Quintero <aq4919@students.necsd.net>
2024-07-17 14:53:01 -0400

    appServer.js

--- server/appServer.js
+++ server/appServer.js
@@ -5,9 +5,15 @@ app.use(express.static('client/public'));
 app.get('/', function(req, res) {
     res.sendFile('index.html', {root: './client/views'})
 })
+
 app.get('/feed', function(req, res) {
     res.sendFile('feed.html', {root: './client/views'})
 })
+
+//API endpoints
+
+const feedRoutes = require('./route/feedRoutes');
+app.use('/api/users', feedRoutes);
 
 
 app.listen(1337, () => console.log('Listening on port 1337.'))
